a9319c1e57d6dab562199dc02ad93bf2629ac3b2
douglasptoliver <douglasptoliver@gmail.com>
2023-09-25 23:58:01 +0000

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   return (
     <>
+    <Card />
       <div className="card">
         <h2>Rick Sanchez</h2>
+
+        <div className="tags">
+        <span className="tag">Status: Vivo</span>
+        <span className="tag">Especie: Humana</span>
+        <span className="tag">Origin: Terra</span>
+        </div>
+
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
     </>
---
+++ src/components/Card.jsx
@@ -0,0 +1,6 @@
+import"./Card.css"
+
+export default function Card() {
+    return <div>Card</div>
+}
+
