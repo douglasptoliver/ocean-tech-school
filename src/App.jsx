887409059ad89a649e604812d779367a73c84460
douglasptoliver <douglasptoliver@gmail.com>
2023-09-26 00:49:54 +0000

    final

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,15 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
+  const item = {
+    nome: 'Rick Sanchez',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+
+  }
   return (
     <>
     <Card />
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-
-        <div className="tags">
-        <span className="tag">Status: Vivo</span>
-        <span className="tag">Especie: Humana</span>
-        <span className="tag">Origin: Terra</span>
-        </div>
-
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,16 @@
 import"./Card.css"
 
 export default function Card() {
-    return <div>Card</div>
+    return <div className="card">
+        <h2>Rick Sanchez</h2>
+
+        <div className="tags">
+        <span className="tag">Status: Vivo</span>
+        <span className="tag">Especie: Humana</span>
+        <span className="tag">Origin: Terra</span>
+        </div>
+
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
 }
 
